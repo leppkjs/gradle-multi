e762fff2f57a6b627673fa3bcbe03fd274914806
unknown <O117012@oad.kcb4u.dom>
2017-12-15 10:07:10 +0900

    webdoc 변경

--- gradle-multi-web/src/main/java/com/kcb/Application.java
+++ gradle-multi-web/src/main/java/com/kcb/Application.java
@@ -1,13 +1,39 @@
 package com.kcb;
 
+import java.io.File;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan("com.kcb")
-public class Application {
+public class Application extends SpringBootServletInitializer {
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+	
+	@Bean
+	public EmbeddedServletContainerCustomizer documentRootConfigurer() {
+	    return new EmbeddedServletContainerCustomizer() {
+			
+			@Override
+			public void customize(ConfigurableEmbeddedServletContainer container) {
+				container.setDocumentRoot(new File("webapp"));
+				
+			}
+		};
+
 	}
 }
--- gradle-multi-web2/src/main/java/com/kcb/Application2.java
+++ gradle-multi-web2/src/main/java/com/kcb/Application2.java
@@ -1,9 +1,14 @@
 package com.kcb;
 
+import java.io.File;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
@@ -17,5 +22,18 @@ public class Application2 extends SpringBootServletInitializer {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(Application2.class, args);
+	}
+	
+	@Bean
+	public EmbeddedServletContainerCustomizer documentRootConfigurer() {
+	    return new EmbeddedServletContainerCustomizer() {
+			
+			@Override
+			public void customize(ConfigurableEmbeddedServletContainer container) {
+				container.setDocumentRoot(new File("webapp"));
+				
+			}
+		};
+
 	}
 }
