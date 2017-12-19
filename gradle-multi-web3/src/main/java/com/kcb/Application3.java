29efceae21494b8db7975a0deb355cb857da3ebe
leppkjs <leppk@naver.com>
2017-12-19 18:30:01 +0900

    aa

---
+++ gradle-multi-web3/src/main/java/com/kcb/Application3.java
@@ -0,0 +1,35 @@
+package com.kcb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan("com.kcb")
+public class Application3 extends SpringBootServletInitializer {
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application3.class);
+    }
+	
+	public static void main(String[] args) {
+		SpringApplication.run(Application3.class, args);
+	}
+	
+	//TODO 스프링부트 임베디드 톰켓한테 강제로 도큐먼트루트를 바라보게 하는 방법
+	/*
+	@Bean
+	public EmbeddedServletContainerCustomizer documentRootConfigurer() {
+	    return new EmbeddedServletContainerCustomizer() {
+	    	@Override
+			public void customize(ConfigurableEmbeddedServletContainer container) {
+				container.setDocumentRoot(new File("build/resources/main"));
+				
+			}
+		};
+	}
+	*/
+}
