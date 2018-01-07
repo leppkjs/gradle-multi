3cfb3a0ce226848aa0633d5795124cffbd3a5ed9
leppkjs <leppk@naver.com>
2018-01-07 22:04:48 +0900

    RSA필터 설정

---
+++ gradle-multi-web/src/main/java/com/kcb/utility/RSARequestWrapper.java
@@ -0,0 +1,81 @@
+package com.kcb.utility;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
+
+import javax.servlet.ReadListener;
+import javax.servlet.ServletInputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+
+import com.google.common.primitives.Bytes;
+
+public class RSARequestWrapper extends HttpServletRequestWrapper {
+	// tag::variables[]
+    private byte[] requestBody = new byte[0];
+    private boolean bufferFilled = false;
+    // end::variables[]
+    
+	public RSARequestWrapper(HttpServletRequest request) {
+		super(request);
+	}
+
+	 // tag::getRequestBody[]
+    public byte[] getRequestBody() throws IOException {
+        if (bufferFilled) {
+            return Arrays.copyOf(requestBody, requestBody.length);
+        }
+
+        InputStream inputStream = super.getInputStream();
+
+        byte[] buffer = new byte[102400]; // 100kb buffer
+
+        int bytesRead;
+        while ((bytesRead = inputStream.read(buffer)) != -1) {
+            requestBody = Bytes.concat(this.requestBody, Arrays.copyOfRange(buffer, 0, bytesRead)); // <1>
+        }
+
+        bufferFilled = true;
+
+        return requestBody;
+    }
+    // end::getRequestBody[]
+
+    // tag::getInputStream[]
+    @Override
+    public ServletInputStream getInputStream() throws IOException {
+        return new CustomServletInputStream(getRequestBody()); // <1>
+    }
+    // end::getInputStream[]
+
+    private static class CustomServletInputStream extends ServletInputStream {
+
+        private ByteArrayInputStream buffer;
+
+        public CustomServletInputStream(byte[] contents) {
+            this.buffer = new ByteArrayInputStream(contents);
+        }
+
+        @Override
+        public int read() throws IOException {
+            return buffer.read();
+        }
+
+        @Override
+        public boolean isFinished() {
+            return buffer.available() == 0;
+        }
+
+        @Override
+        public boolean isReady() {
+            return true;
+        }
+
+        @Override
+        public void setReadListener(ReadListener listener) {
+            throw new RuntimeException("Not implemented");
+        }
+    }
+}
